9f1c660e7c2b0b8d8626bdf8395d86ad358d1aa8
Berint Moffett <berint.moffett@gmail.com>
2024-03-07 09:58:06 -0700

    minor reorganization

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,5 +1,6 @@
-use std::rc::Rc;
-use crate::token::Token;
+mod node;
+
+pub use node::*;
 
 pub struct Program {
     pub statements: Vec<Node>,
@@ -27,53 +28,3 @@ pub enum Expression {
     InfixExpression(InfixExpression),
 }
 
-#[derive(Debug)]
-pub struct LetStatement {
-    pub token: Token,
-    pub name: Identifier,
-    pub value: Rc<Expression>,
-}
-
-#[derive(Debug)]
-pub struct ReturnStatement {
-    pub token: Token,
-    pub return_value: Rc<Expression>,
-}
-
-#[derive(Debug)]
-pub struct ExpressionStatement {
-    pub token: Token,
-    pub expression: Rc<Expression>,
-}
-
-#[derive(Debug)]
-pub struct Identifier {
-    pub token: Token,
-}
-
-impl Identifier {
-    pub fn value(&self) -> &str {
-        self.token.literal.as_str()
-    }
-}
-
-#[derive(Debug)]
-pub struct IntegerLiteral {
-    pub token: Token,
-    pub value: i64,
-}
-
-#[derive(Debug)]
-pub struct PrefixExpression {
-    pub token: Token,
-    pub operator: String,
-    pub right: Rc<Expression>,
-}
-
-#[derive(Debug)]
-pub struct InfixExpression {
-    pub token: Token,
-    pub left: Rc<Expression>,
-    pub operator: String,
-    pub right: Rc<Expression>,
-}
